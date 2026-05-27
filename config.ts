8478947ad3961fe9e931eafcb59f232c230b7e9d
IllyaX <en4anter213@gmail.com>
2026-05-27 22:16:21 +0300

    fix: full build reset without android folder

---
+++ config.ts
@@ -0,0 +1,20 @@
+export const SUPABASE_CONFIG = {
+  ASSETS_URL: 'https://ikjxctiecledoxeeecdi.storage.supabase.co/storage/v1/object/public/assets',
+
+  CAMPAIGN_MATERIALS_URL: 'https://ikjxctiecledoxeeecdi.storage.supabase.co/storage/v1'
+};
+
+export const SUPABASE_URL = 'https://tvgscwobfscaqdfpydid.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_Y9MSj3n9yNytShxkveTj1Q_czgovCLZ';
+
+export const LOCAL_CONFIG = {
+  BASE_URL_IP: 'https://aether-backend-hbkf.onrender.com',
+  BASE_URL_IP_API: 'https://aether-backend-hbkf.onrender.com/api'
+
+  // BASE_URL_IP: 'http://192.168.0.101:3000',
+  // BASE_URL_IP_API: 'http://192.168.0.101:3000/api'
+};
+
+export const CURRENT_SESSION = {
+  userName: '',
+};
